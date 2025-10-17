ecaeec281035209d1f5bfdf2b6d55a4abf969a7b
Daniyar Kshibekov <danik30032005@gmail.com>
2025-10-17 17:19:29 +0500

    Update

--- swe_project/main/models.py
+++ swe_project/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    price = models.FloatField()
+    weight = models.FloatField()
+    quantity = models.IntegerField()
+    supplierID = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Request(models.Model):
+    consumerID = models.IntegerField()
+    supplierID = models.IntegerField()
+    status = models.CharField(max_length=50)
+    requestDate = models.DateField(auto_now_add=True)
+    consumerComment = models.TextField()
+
+    def __str__(self):
+        return f"{self.consumerID} → {self.supplierID} ({self.status})"
