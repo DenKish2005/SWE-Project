72c7337ba58c1192a9f9a31a9211f807077730a8
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-12 23:37:55 +0500

    UPDATE

--- swe_project/main/admin.py
+++ swe_project/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Item, Request
+from .models import Item, Request, Category
 
 admin.site.register(Item)
 admin.site.register(Request)
+admin.site.register(Category)
