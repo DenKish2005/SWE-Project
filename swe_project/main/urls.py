304b7c94d72eed79b6e9b8babf2554f1d155f6b6
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-12 23:40:28 +0500

    UPDATE

--- swe_project/main/urls.py
+++ swe_project/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, RequestViewSet
+from .views import ItemViewSet, RequestViewSet, CategoryViewSet
 
 router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
 router.register(r'items', ItemViewSet)
 router.register(r'requests', RequestViewSet)
 
