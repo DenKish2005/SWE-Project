a68125e5f1534b692d349e696fbb5797efc232c1
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-12 23:37:03 +0500

    UPDATE

--- swe_project/main/serializers.py
+++ swe_project/main/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Item, Request
+from .models import Item, Request, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
