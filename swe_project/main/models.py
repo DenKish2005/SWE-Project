054c874509cbeebd27b720bf6b2aa9ee16ed949a
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-19 20:14:01 +0500

    UPDATE

--- swe_project/main/models.py
+++ swe_project/main/models.py
@@ -3,11 +3,59 @@ from django.db import models
 from django.db.models import Q
 from django.conf import settings
 
+class UserProfile(models.Model):
+    class Role(models.TextChoices):
+        CONSUMER = "consumer", "Consumer"
+        SUPPLIER_OWNER = "owner", "Owner"
+        SUPPLIER_MANAGER = "manager", "Manager"
+        SALES = "sales", "Sales"
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=Role.choices)
+
+class Supplier(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # владелец
+    company_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=100, blank=True)
+
+class Consumer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=255, blank=True)
+
+class SupplierStaff(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="staff")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=UserProfile.Role.choices)
+
+class SupplierConsumerLink(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "Pending", "Pending"
+        APPROVED = "Approved", "Approved"
+        REJECTED = "Rejected", "Rejected"
+
+    consumer = models.ForeignKey(Consumer, on_delete=models.CASCADE, db_index=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, db_index=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING, db_index=True)
+    requested_at = models.DateTimeField(auto_now_add=True)
+    approved_at = models.DateTimeField(null=True, blank=True)
+    rejected_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['consumer', 'supplier', 'status'],
+                name='uniq_active_link',
+                condition=Q(status__in=['Pending', 'Approved'])
+            ),
+        ]
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     def __str__(self): return self.name
 
 class Item(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="items", db_index=True)
     name = models.CharField(max_length=255, db_index=True)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=12, decimal_places=2,
@@ -68,48 +116,3 @@ class Message(models.Model):
 
     def __str__(self): return f"{self.senderType}: {self.text[:30]}"
 
-class UserProfile(models.Model):
-    class Role(models.TextChoices):
-        CONSUMER = "consumer", "Consumer"
-        SUPPLIER_OWNER = "owner", "Owner"
-        SUPPLIER_MANAGER = "manager", "Manager"
-        SALES = "sales", "Sales"
-
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=Role.choices)
-
-class Supplier(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # владелец
-    company_name = models.CharField(max_length=255)
-    city = models.CharField(max_length=100, blank=True)
-
-class Consumer(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    business_name = models.CharField(max_length=255, blank=True)
-
-class SupplierStaff(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="staff")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=UserProfile.Role.choices)
-
-class SupplierConsumerLink(models.Model):
-    class Status(models.TextChoices):
-        PENDING = "Pending", "Pending"
-        APPROVED = "Approved", "Approved"
-        REJECTED = "Rejected", "Rejected"
-
-    consumer = models.ForeignKey(Consumer, on_delete=models.CASCADE, db_index=True)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, db_index=True)
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING, db_index=True)
-    requested_at = models.DateTimeField(auto_now_add=True)
-    approved_at = models.DateTimeField(null=True, blank=True)
-    rejected_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['consumer', 'supplier', 'status'],
-                name='uniq_active_link',
-                condition=Q(status__in=['Pending', 'Approved'])
-            ),
-        ]
