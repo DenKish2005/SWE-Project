836330681d902343c3351751f8ef42a5a7cc18cd
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-22 07:09:52 +0500

    UPDATE

--- swe_project/main/admin.py
+++ swe_project/main/admin.py
@@ -1,23 +1,24 @@
 from django.contrib import admin
-from .models import Item, Request, Category, Message
+from .models import Item, Category, Message, Attachment, Supplier, Consumer, SupplierStaff, SupplierConsumerLink, Order, OrderItem, Incident, Notification, SupplierKYB
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ("id","name","supplierID","price","quantity","category")
-    list_filter = ("category",)
-    search_fields = ("name","description")
-
-@admin.register(Request)
-class RequestAdmin(admin.ModelAdmin):
-    list_display = ("id","consumerID","supplierID","status","requestDate")
-    list_filter = ("status",)
-    search_fields = ("consumerComment",)
+    list_display = ("id", "name", "supplier", "price", "stock_level", "category", "is_available")
+    list_filter = ("category", "is_available")
+    search_fields = ("name", "description")
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "parent", "is_moderated", "created_at")
     search_fields = ("name",)
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ("id","request","senderType","created_at")
-    list_filter = ("senderType",)
+    list_display = ("id", "link", "order", "sender_type", "created_at")
+    list_filter = ("sender_type",)
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "message", "mime_type", "size", "created_at")
+
+admin.site.register((Supplier, Consumer, SupplierStaff, SupplierConsumerLink, Order, OrderItem, Incident, Notification, SupplierKYB))
