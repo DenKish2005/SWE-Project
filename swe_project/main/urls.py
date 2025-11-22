f797d16553fed9c8dfd4ac9d90c3c1b1c666fe69
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-22 07:17:44 +0500

    UPDATE

--- swe_project/main/urls.py
+++ swe_project/main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, RequestViewSet, CategoryViewSet, MessageViewSet
-from .views import SupplierViewSet, ConsumerViewSet, SupplierStaffViewSet
-from .views import SupplierConsumerLinkViewSet, OrderViewSet, IncidentViewSet, NotificationViewSet
+from .views import (
+    ItemViewSet, CategoryViewSet, MessageViewSet,
+    SupplierViewSet, ConsumerViewSet, SupplierStaffViewSet,
+    SupplierConsumerLinkViewSet, OrderViewSet, IncidentViewSet, NotificationViewSet,
+    KYBViewSet
+)
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'items', ItemViewSet)
-router.register(r'requests', RequestViewSet)
 router.register(r'messages', MessageViewSet)
 router.register(r'suppliers', SupplierViewSet)
 router.register(r'consumers', ConsumerViewSet)
@@ -16,8 +18,6 @@ router.register(r'links', SupplierConsumerLinkViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'incidents', IncidentViewSet)
 router.register(r'notifications', NotificationViewSet)
+router.register(r'platadmin/kyb', KYBViewSet)
 
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = [ path('', include(router.urls)), ]
