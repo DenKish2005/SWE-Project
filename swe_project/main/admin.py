9558d995526cf44789e873b0c5b4aa452b26cfbe
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-22 07:10:05 +0500

    UPDATE

--- swe_project/main/admin.py
+++ swe_project/main/admin.py
@@ -21,4 +21,5 @@ class MessageAdmin(admin.ModelAdmin):
 class AttachmentAdmin(admin.ModelAdmin):
     list_display = ("id", "message", "mime_type", "size", "created_at")
 
-admin.site.register((Supplier, Consumer, SupplierStaff, SupplierConsumerLink, Order, OrderItem, Incident, Notification, SupplierKYB))
+admin.site.register((Supplier, Consumer, SupplierStaff, SupplierConsumerLink, 
+                     Order, OrderItem, Incident, Notification, SupplierKYB))
