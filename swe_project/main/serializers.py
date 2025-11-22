a6a46bdcf525afadccbbe1e56bb130e7f5336496
Daniyar Kshibekov <danik30032005@gmail.com>
2025-11-22 05:09:16 +0500

    UPDATE

--- swe_project/main/serializers.py
+++ swe_project/main/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import Item, Request, Category, Message
+from .models import (
+    Supplier, Consumer, SupplierStaff, SupplierConsumerLink,
+    Order, OrderItem, Incident, Notification
+)
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +25,43 @@ class RequestSerializer(serializers.ModelSerializer):
         model = Request
         fields = '__all__'
 
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+class ConsumerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consumer
+        fields = '__all__'
+
+class SupplierStaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SupplierStaff
+        fields = '__all__'
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SupplierConsumerLink
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class IncidentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Incident
+        fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
